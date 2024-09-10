37aca3a48162ff66668d9611d46f81b091d68326
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2024-09-10 11:02:14 -0400

    feat: adjust store module and store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {}), // Inicializa o Store sem reducers por enquanto
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Mantém o histórico das últimas 25 ações
+      logOnly: environment.production, // Em produção logar apenas ações
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
