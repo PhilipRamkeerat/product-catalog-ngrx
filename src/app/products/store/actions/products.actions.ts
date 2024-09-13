77bb0a27c4546192d0d137d2fa0ec7f24dc3de28
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2024-09-13 12:28:04 -0400

    feat: change action addProduct name

--- src/app/products/store/actions/products.actions.ts
+++ src/app/products/store/actions/products.actions.ts
@@ -2,6 +2,6 @@ import { createAction, props } from '@ngrx/store';
 import { Product } from '../../models/product.model';
 
 export const loadProducts = createAction('[Products] Load Products');
-export const addProducts = createAction('[Products] Add Products', props<{ product: Product }>());
+export const addProduct = createAction('[Product] Add Product', props<{ product: Product }>());
 export const removeProduct = createAction('[Products] Remove Product', props<{ id: number }>());
 export const updateProduct = createAction('[Products] Update Product', props<{ id: number; changes: Partial<Product> }>());
